e83a05fcfe95c868781097a0b2723914d3f44897
Tomas Taboada <tomastaboada27@gmail.com>
2026-08-10 23:08:20 -0300

    Update EnemyCombatAIController.cs

--- Project Grimhold/Assets/Scripts/Player/Combat/EnemyCombatAIController.cs
+++ Project Grimhold/Assets/Scripts/Player/Combat/EnemyCombatAIController.cs
@@ -179,6 +179,9 @@ public sealed class EnemyCombatAIController : NetworkBehaviour, ICombatControlle
         );
         _pendingTargetId = targetId;
         _hasPendingDamage = true;
+
+        // Bypassing animation event for Ranged enemies since they do not have attack animations yet.
+        if (_activeAttack != null && _activeAttack.Type == AttackType.Ranged)
         {
             ExecutePendingDamage();
         }
